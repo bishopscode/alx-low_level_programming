fbdfa0b1d0031c4b411488bf0df07003311881f7
Moses Nebechukwu <bishopslow01@gmail.com>
2023-08-11 00:57:35 +0100

    using put function to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
+   #include <stdio.h>
 
+   /**
+    *main - Entry point
+    *
+    *Return: Always 0 (success)
+    */
+
+    int main(void)
+    {
+        char myString[] = "Programming is like building a multilingual puzzle";
+	put (myString); //outputs "Programming is like building a multilingual puzzle" followed by a new line
+	return (0);
+    }  	
