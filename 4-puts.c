3b6cde0b8f2a30775cbb23c266c92e96faa2668a
Moses Nebechukwu <bishopslow01@gmail.com>
2023-08-11 21:18:16 +0100

    using put function to print a string

--- 4-puts.c
+++ 4-puts.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	char myString[] = "Programming is like building a multilingual puzzle";
-
-	puts(myString);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
