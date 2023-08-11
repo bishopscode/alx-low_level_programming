51b3e3fc092d18600c71a164554b686b624b007c
Moses Nebechukwu <bishopslow01@gmail.com>
2023-08-11 15:00:24 -0700

    implementing put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
    /**
     *main - Entry point
-    *
+    *This function is the entry point of the program. It demonstrates the usage
+    * of the `puts` function to print a string to the standard output.
     *Return: Always 0 (success)
     */
 int main(void)
 {
+	/*using the 'put' function to print a string*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
