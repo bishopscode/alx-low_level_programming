58e17806e7c4b463c8406c3f0db6c153fa9e4933
Moses Nebechukwu <bishopslow01@gmail.com>
2023-08-13 21:15:55 +0100

    print small-bigalpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,33 @@
-#!/bin/bash
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: A program that prints the alphabet in lowercase
+ * and uppercases right next to it, followed by a new line.
+ * Return: return 0 always (success)
+ */
+
+int main(void)
+{
+		char lowercase = 'a';
+			char uppercase = 'A';
+
+				while
+					(lowercase <= 'z')
+				{
+						putchar(lowercase);
+							lowercase++;
+				}
+				while
+					(uppercase <= 'Z')
+					{
+						putchar(uppercase);
+							uppercase++;
+					}
+						putchar('\n');
+
+							return (0);
+
+}
