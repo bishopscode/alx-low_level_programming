af2f20dbf63c2b1fa7d4edc9fcfb2f20302fd3fb
Moses Nebechukwu <bishopslow01@gmail.com>
2023-08-14 12:50:04 +0100

    reverse print of the alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,26 @@
-#!/bin/bash
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: A program that prints the alphabet in lowercase
+ * followed by a new line.
+ * Return: return 0 always (success)
+ */
+
+int main(void)
+{
+		char ch = 'z';
+
+			while
+				(ch >= 'a')
+			{
+					putchar(ch);
+						ch--;
+			}
+				putchar('\n');
+
+			return (0);
+
+}
