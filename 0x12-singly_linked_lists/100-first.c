d21363d93f6e6ac6f05cc10a7e2d060be063e074
Moses Nebechukwu <bishopslow01@gmail.com>
2023-09-21 15:58:40 +0100

    function that prints a pre main-mssg

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * pre_main_message - Prints the pre-main message.
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
